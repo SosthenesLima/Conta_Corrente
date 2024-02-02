8d433f92884f92a9d5fd0b4fdc381f35bb80ee27
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-02-01 22:20:08 -0300

    Commit - Super/Override em classes filhas

--- MyPlayground_Heranca.playground/Contents.swift
+++ MyPlayground_Heranca.playground/Contents.swift
@@ -1,3 +1,63 @@
-import UIKit
+/* 
+ Autor: Sósthenes Oliveira Lima
+ 01/02/2024
+ Herança e o uso do Super/ Override em classes filhas.
+*/
+class Conta {
+    var saldo = 0.0
+    var nome: String
+    
+    func sacar(_ valor: Double) {
+        saldo -= valor
+        
+    }
+    func depositar(_ valor: Double) {
+        saldo += valor
+    }
+    init(nome: String){
+        self.nome = nome
+    }
+}
 
-var greeting = "Hello, playground"
+class ContaPoupanca: Conta {
+    var possuiCartaoDebito: Bool
+
+    func solicitarDebito() {
+        possuiCartaoDebito = true
+        print("O Cliente está solicitando cartão de Débito")
+    }
+    override func sacar(_ valor: Double) {
+        saldo -= valor + 10
+    }
+
+    init(nome: String, possuiCartaoDebito: Bool) {
+        self.possuiCartaoDebito = possuiCartaoDebito
+        super.init(nome: nome)
+    }
+}
+
+var contaPoupanca = ContaPoupanca(nome: "Sósthenes", possuiCartaoDebito: false)
+contaPoupanca.depositar(50)
+contaPoupanca.sacar(20)
+print(contaPoupanca.saldo)
+contaPoupanca.solicitarDebito()
+
+class ContaCorrente: Conta {
+    func solicitarEmprestimo(_ valor: Double){
+        print("O cliente está solicitando um empréstimo no valor de \(valor)")
+        super.depositar(valor)
+    }
+    
+    override func sacar(_ valor: Double) {
+        saldo -= valor + 5
+    }
+}
+
+var contaCorrente = ContaCorrente(nome: "Catherine")
+contaCorrente.depositar(100)
+contaCorrente.sacar(20)
+print(contaCorrente.saldo)
+contaCorrente.solicitarEmprestimo(20000)
+print(contaCorrente.saldo)
+
+
