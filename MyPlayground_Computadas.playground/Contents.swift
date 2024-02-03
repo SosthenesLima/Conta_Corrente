8ba480018ca8f033356d93aab1cddd9d1976e6ad
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-02-03 15:09:08 -0300

    commit - estáticas

--- MyPlayground_Computadas.playground/Contents.swift
+++ MyPlayground_Computadas.playground/Contents.swift
@@ -1,3 +1,36 @@
-import UIKit
+/*
+ Propriedade Computada
+ Autor: Sósthenes Oliveira Lima
+ 03/02/2024
+ 
+ */
 
-var greeting = "Hello, playground"
+class Conta {
+    var saldo = 0.0
+    var nome: String
+    
+    static var taxaTransferencia = 0.1
+    
+    var negativado: Bool {
+        return saldo < 0
+    }
+    
+    func sacar (_ valor: Double) {
+        saldo -= valor
+     }
+    
+    func depositar(_ valor: Double) {
+        saldo += valor
+     }
+    
+    init( nome: String) {
+        self.nome = nome
+    }
+}
+
+var contaSosthens = Conta(nome: "Sósthenes")
+contaSosthens.sacar(20)
+print(contaSosthens.negativado)
+contaSosthens.depositar(100)
+print(contaSosthens.negativado)
+print(Conta.taxaTransferencia)
