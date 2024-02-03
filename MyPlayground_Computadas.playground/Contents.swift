0e455781b6f46e8f5109e9a88b5b277373ff1ce7
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-02-03 15:32:14 -0300

    Commit Observadores de propriedade

--- MyPlayground_Computadas.playground/Contents.swift
+++ MyPlayground_Computadas.playground/Contents.swift
@@ -1,12 +1,20 @@
 /*
  Propriedade Computada
+ Observadores de propriedade
  Autor: Sósthenes Oliveira Lima
  03/02/2024
  
  */
 
 class Conta {
-    var saldo = 0.0
+    var saldo = 0.0 {
+        willSet(novoValor) {
+            print("O saldo está sendo alterado! O seu novo valor será de \(novoValor)")
+        }
+        didSet(valorAntigo) {
+            print("O seu saldo está sendo alterado! O seu valor antigo era de \(valorAntigo)")
+        }
+    }
     var nome: String
     
     static var taxaTransferencia = 0.1
@@ -29,8 +37,9 @@ class Conta {
 }
 
 var contaSosthens = Conta(nome: "Sósthenes")
-contaSosthens.sacar(20)
-print(contaSosthens.negativado)
+contaSosthens.depositar(20)
+/*print(contaSosthens.negativado)
 contaSosthens.depositar(100)
 print(contaSosthens.negativado)
 print(Conta.taxaTransferencia)
+*/
--- MyPlayground_Estudante.playground/Contents.swift
+++ MyPlayground_Estudante.playground/Contents.swift
@@ -1,3 +1,22 @@
-import UIKit
+/*
+ Será exibido no console o valor 3 e a propriedade contagemEstudantes é uma propriedade estática.
+ 
+ 
+ */
 
-var greeting = "Hello, playground"
+class Estudante {
+    var nome: String
+    var idade: Int
+    static var contagemEstudantes = 0
+
+    init(nome: String, idade: Int) {
+        self.nome = nome
+        self.idade = idade
+        Estudante.contagemEstudantes += 1
+    }
+}
+
+let estudante1 = Estudante(nome: "João", idade: 28)
+let estudante2 = Estudante(nome: "Carlos", idade: 32)
+let estudante3 = Estudante(nome: "Ana", idade: 29)
+print(Estudante.contagemEstudantes) // O que será exibido?
