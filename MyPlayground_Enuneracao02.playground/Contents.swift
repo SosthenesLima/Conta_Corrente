27c82c8b647b05567a4c68621f410ea8a5e5d069
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-02-04 16:37:36 -0300

    Commit - Raw Values03

--- MyPlayground_Enuneracao02.playground/Contents.swift
+++ MyPlayground_Enuneracao02.playground/Contents.swift
@@ -5,10 +5,21 @@
  
  */
 
-enum Mes: Int {
-    case janeiro , fevereiro, marco, abril, maio, junho, julho, agosto, setembro, outubro, novembro, dezembro
+enum Mes: String {
+    case janeiro = "JANEIRO" , fevereiro, marco, abril, maio, junho, julho, agosto, setembro, outubro, novembro, dezembro
 }
 var janeiro = Mes.janeiro
 print(janeiro.rawValue)
 var fevereiro: Mes = .fevereiro
 print(fevereiro.rawValue)
+
+enum Moeda: Double {
+    case umCentavo = 0.01
+    case cincoCentavos = 0.05
+    case dezCentavos = 0.10
+    case vinteCincoCentavos = 0.25
+    case cinquetaCentavos = 0.50
+}
+
+var moedacincoCentavos = Moeda.cincoCentavos
+print(moedacincoCentavos.rawValue)
